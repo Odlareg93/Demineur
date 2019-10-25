79d7836ea972b40779cb1fdb067f68845889d710
Jules Saunier <julessaunier@hotmail.fr>
2019-10-25 09:36:36 +0200

    nouveau fichier : demineur 	modifié :         demineur.c

--- demineur.c
+++ demineur.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-define H 20
-define W 20
+struct scase {
+   char val;
+   int isHidden;
+};
+
+
+void main(){
+
+    struct scase tab[20][20];
+   
+    for(int a = 0;a<20;a++){
+        for(int z = 0;z<20;z++){
+            tab[a][z].val = '0';
+            tab[a][z].isHidden = 0;
+        }
+    }
+
+
+    
+
+    for(int a = 0;a<20;a++){
+        for(int z = 0;z<20;z++){
+            printf("\t%c",tab[a][z].val);
+        }
+    printf("\n");
+    }
+
+}
+
+
