713c6ec0a106caa4be98db60944028044890d12b
Nkaruna00 <nithushankarunakaran@gmail.com>
2019-10-25 16:51:02 +0200

    Identation

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Ce projet est un jeu de démineur 20*20 \n
 "cmake CMakeLists.txt" pour pour utiliser CMake \n
 Puis lancer "make" \n
-Executer "Demineur" dans /bin
+Executer "Demineur" dans /bin\n
+
+Dans le cas où il y a un problème avec Cmake , effacer CMakeCache et CMakeLog
--- src/main.c
+++ src/main.c
@@ -11,49 +11,53 @@
 int
 main ()
 {
-  int hidden,mine,r;
+  int hidden, mine, r;
   bool end = false;
   scase tab[20][20];
-  fill(tab);
-  putMine(tab);
-  setVal(tab);
-  hidden = getHidden(tab);
-  printf("%d : hidden\n",hidden);
-  mine = getMine(tab);
-  printf("%d mine\n",mine);
-  printf("%d : is WIn\n",isWin(tab));
+  fill (tab);
+  putMine (tab);
+  setVal (tab);
+  hidden = getHidden (tab);
+  printf ("%d : hidden\n", hidden);
+  mine = getMine (tab);
+  printf ("%d mine\n", mine);
+  printf ("%d : is WIn\n", isWin (tab));
 
-  while (end != true){
-    char inputChar;
-    int x, y;
+  while (end != true)
+    {
+      char inputChar;
+      int x, y;
 
-    print_tableau (tab);
-    /*printf("Entrer 'q'  pour quitter: ");
-    scanf("%c", &inputChar);
+      print_tableau (tab);
+      /*printf("Entrer 'q'  pour quitter: ");
+         scanf("%c", &inputChar);
 
-    if (inputChar == 'q'){
-      exit(-1);
-    }*/
+         if (inputChar == 'q'){
+         exit(-1);
+         } */
 
-    printf("Entrer votre numéro de cologne x: ");
-    scanf("%d", &x);
+      printf ("Entrer votre numéro de cologne x: ");
+      scanf ("%d", &x);
 
-    printf("Entrer votre numéro de cologne y: ");
-    scanf("%d", &y);
+      printf ("Entrer votre numéro de cologne y: ");
+      scanf ("%d", &y);
 
-    for(int i =0;i < 20;++i){
-      for(int j = 0; j< 20;++j){
-      //printf("%c",tab[i][j].val);
+      for (int i = 0; i < 20; ++i)
+	{
+	  for (int j = 0; j < 20; ++j)
+	    {
+	      //printf("%c",tab[i][j].val);
+	    }
+	}
+
+      play (x, y, tab);
+      if (isWin (tab))
+	{
+	  victoire ();
+	}
+
+
+
     }
-    }
-
-    play(x, y, tab);
-    if(isWin(tab)){
-      victoire();
-    }
-
-
-
-  }
   return 0;
 }
