9e1a388becafa6e88ed18dad46a5b49a3d04d1ad
Jules Saunier <julessaunier@hotmail.fr>
2019-10-25 11:12:27 +0200

    modifié :         demineur 	modifié :         demineur.c

--- demineur.c
+++ demineur.c
@@ -13,12 +13,14 @@ void main(){
 
     putMine();
     
+    setVal();
+    
     aff();
 
 
 }
 
-void fill()
+void fill() // Remplis le tableau de case vide et cachés
 {
     for(int a = 0;a<20;a++){
         for(int z = 0;z<20;z++){
@@ -28,16 +30,40 @@ void fill()
     }
 }
 
-void aff(){
+void aff(){ // Affiche le tableau
     for(int a = 0;a<20;a++){
         for(int z = 0;z<20;z++){
-            printf("\t%c",tab[a][z].val);
+            printf(" %c",tab[a][z].val);
         }
     printf("\n");
     }
 }
 
-void putMine(){
+void setVal(){ // Determine la valeur de la case en fonction du nombre de mine autour
+    int nbVal=0;
+    for(int a = 0;a<20;a++){
+        for(int z = 0;z<20;z++){
+            nbVal=0;
+            if(tab[a][z].val == 'x'){
+                break;
+            }
+    
+            if(tab[a-1][z].val == 'x' ) nbVal++;
+            if(tab[a][z-1].val == 'x' ) nbVal++;
+            if(tab[a+1][z].val == 'x' ) nbVal++;
+            if(tab[a][z+1].val == 'x' ) nbVal++;
+            if(tab[a-1][z-1].val == 'x' ) nbVal++;
+            if(tab[a+1][z+1].val == 'x' ) nbVal++;
+            if(tab[a-1][z+1].val == 'x' ) nbVal++;
+            if(tab[a+1][z-1].val == 'x' ) nbVal++;
+        
+            char c = nbVal+'0';
+            tab[a][z].val =  c;
+        }
+    }
+}
+
+void putMine(){ // Place les mines de manière aléatoires
 
     for(int a=0;a<40;a++)
     {
@@ -47,5 +73,4 @@ void putMine(){
         tab[u][y].val = 'x';
         printf("1 : %d 2 : %d \n",u,y);
     }
-    
 }
