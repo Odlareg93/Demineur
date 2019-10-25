3bc73b35d8109d721bd3dcf5172b2a1dc723dc90
Jules Saunier <julessaunier@hotmail.fr>
2019-10-25 10:41:18 +0200

    modifié :         demineur 	modifié :         demineur.c

--- demineur.c
+++ demineur.c
@@ -5,37 +5,47 @@ struct scase {
    int isHidden;
 };
 
+struct scase tab[20][20];
 
 void main(){
+ 
+    fill();
 
-    struct scase tab[20][20];
-   
+    putMine();
+    
+    aff();
+
+
+}
+
+void fill()
+{
     for(int a = 0;a<20;a++){
         for(int z = 0;z<20;z++){
             tab[a][z].val = '0';
-            tab[a][z].isHidden = 0;
+            tab[a][z].isHidden = 1;
         }
     }
+}
 
-    int tab[40][2];
-    for(int a=0;a<40;a++)
-    {
-        srand (time (NULL)); 
-        tab[a][0] = Random();
-        tab[a][1] = Random();
-    }
-
+void aff(){
     for(int a = 0;a<20;a++){
         for(int z = 0;z<20;z++){
             printf("\t%c",tab[a][z].val);
         }
     printf("\n");
     }
-
 }
 
-int Random ()
-{
-return ((rand () % (21)));
-} 
+void putMine(){
 
+    for(int a=0;a<40;a++)
+    {
+        
+        int u = rand()%20+1;
+        int y = rand()%20+1;
+        tab[u][y].val = 'x';
+        printf("1 : %d 2 : %d \n",u,y);
+    }
+    
+}
